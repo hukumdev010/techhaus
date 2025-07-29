f85573bcdda5be1074319e77e6549432420790a1
hukum basnet <basnethukum789@gmail.com>
2025-07-29 16:48:49 +0545

    Add cors

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import path from "path";
 import taskrouter from "./routes/task";
+import cors from "cors";
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
-app.use;
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
